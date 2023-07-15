31f9a5caf87c5e74b7908b77080137acf5310520
Anees Sahla <s17140@sci.pdn.ac.lk>
2023-07-15 20:20:36 +0530

    First commit

---
+++ eclipse-workspace/automation-testing/src/test/java/com/example/testcases/Login.java
@@ -0,0 +1,43 @@
+package com.example.testcases;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import com.example.config.Constants;
+
+public class Login {
+    private WebDriver driver;
+    private By usernameField = By.id("username");
+    private By passwordField = By.name("pwd");
+    private By loginButton = By.id("loginButton");
+
+    public Login(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void open() {
+        driver.get(Constants.WEB_URL);
+        driver.manage().window().maximize();
+    }
+
+    public void setUsername(String username) {
+        driver.findElement(usernameField).sendKeys(username);
+    }
+
+    public void setPassword(String password) {
+        driver.findElement(passwordField).sendKeys(password);
+    }
+
+    public void clickLoginButton() {
+        driver.findElement(loginButton).click();
+    }
+}
+
